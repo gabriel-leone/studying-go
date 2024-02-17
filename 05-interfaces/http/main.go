aa1a22c572c3e840e7b3903d304cf7a4119c923c
Gabriel Leone <leonepgabriel@gmail.com>
2024-02-16 22:05:07 -0300

    feat: Estudo acerca de conceitos tais quais: Maps, interfaces, http requests, Reader and Writer functions.

---
+++ 05-interfaces/http/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		println("Error:", err)
+		os.Exit(1)
+	}
+
+	io.Copy(os.Stdout, resp.Body)
+}
